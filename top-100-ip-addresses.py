b04ba3d87706cb58b258ff9df29f9aa0885b0e90
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2021-03-04 22:34:59 -0800

    updated requestHandled function to create a new key/value pair if the ip address doesn't exist. if it does exist, increment the value by 1

--- top-100-ip-addresses.py
+++ top-100-ip-addresses.py
@@ -5,8 +5,13 @@ from collections import defaultdict
 # If the key does exist then do not create a new key/value pair
 ipAddresses = defaultdict(int)
 
-# function to take in ip address
-# def request_handled(ip_address):
+def requestHandled(ipAddress):
+    '''
+    Function that takes in one ip address at a time and assigns the key to the ip address
+    If the ip address doesn't exist already in the dictionary, then create value of 1
+    If the ip address does exist, increment the value by 1
+    '''
+    ipAddresses[ipAddress] += 1
 
 # function to return top 100 ip addresses by count, with highest traffic ip address first
 # def top100():
