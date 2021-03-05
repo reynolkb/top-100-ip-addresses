e4a0de596215d0963d31b6d8f45e7f7e1313f25d
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2021-03-04 22:23:43 -0800

    changed dictionary to default dictionary to allow for better data management. updated comments with more detail

--- top-100-ip-addresses.py
+++ top-100-ip-addresses.py
@@ -1,5 +1,9 @@
-# global dictionary to store ip addresses and request count
-# ipAddresses = {}
+from collections import defaultdict
+
+# Create a default dictionary sortable by value, all the values are integers
+# Default dictionary allows you to add a new key/value pair only if the key doesn't already exists
+# If the key does exist then do not create a new key/value pair
+ipAddresses = defaultdict(int)
 
 # function to take in ip address
 # def request_handled(ip_address):
