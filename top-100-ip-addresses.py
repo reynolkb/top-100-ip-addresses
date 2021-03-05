17f69a229f651c340d3483f46fce6d444f8b92e9
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2021-03-04 23:10:26 -0800

    imported time and created startTime to calculate how long it takes request_handled() and top100() functions to run

--- top-100-ip-addresses.py
+++ top-100-ip-addresses.py
@@ -1,5 +1,7 @@
+# import defaultdict, random for int generation and time for printing out how long each function takes to run
 from collections import defaultdict
 import random
+import time
 
 # Create a default dictionary sortable by value, all the values are integers
 # Default dictionary allows you to add a new key/value pair only if the key doesn't already exists
@@ -35,13 +37,25 @@ def run():
     I used random integers instead of random ip addresses for testing so I could create multiple requests from the same integer
     I did convert these integers into strings
     '''
+    # StartTime for run function
+    startTime = time.time()
+    
     # Call requestHandled with random ip adresses ranging from 1 - 1 million, 10 million times
     for _ in range(10000000):
         ip = str(random.randint(1,1000000))
         requestHandled(ip)
     
+    # Print how long it took to run request_handled() function
+    print('request_handled() called 10,000,000 times ' + str(time.time() - startTime))
+
+    # Reintialize startTime
+    startTime = time.time()
+    
     # Call top100 to get the ip addresses with the most requests
     top100()
+
+    # Print how long it took to run top100() function
+    print('top100() called ' + str(time.time() - startTime))
 
     # Clear out the dictionary and print to show it's cleared
     print('clearing out dictionary')
