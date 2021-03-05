481c4d34f07afda82001433b0ba4b360d5ff2398
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2021-03-04 22:16:00 -0800

    added base code with comments and readme with questions

---
+++ top-100-ip-addresses.py
@@ -0,0 +1,11 @@
+# global dictionary to store ip addresses and request count
+# ipAddresses = {}
+
+# function to take in ip address
+# def request_handled(ip_address):
+
+# function to return top 100 ip addresses by count, with highest traffic ip address first
+# def top100():
+
+# clear ip addresses and count at the start of each day
+# def clear():
